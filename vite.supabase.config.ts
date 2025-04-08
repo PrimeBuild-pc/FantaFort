f0987d6fb90fb376cdbdfa2a0519c4cf51b3cc95
Prime Build <primebuild.official@gmail.com>
2025-04-08 18:06:54 +0200

    Aggiunte dipendenze al file di configurazione

--- vite.supabase.config.ts
+++ vite.supabase.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
       input: path.resolve(__dirname, './client/index.html'),
     },
   },
-  define: {
-    'process.env.VITE_SUPABASE_URL': JSON.stringify('https://nxrqxozgbjiegjqgjypa.supabase.co'),
-    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54cnF4b3pnYmppZWdqcWdqeXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxMDcyNTgsImV4cCI6MjA1OTY4MzI1OH0.se5REjhJrxPW_7hSKNvdeJ_IW09OPs1iTOrM8FKZ67s'),
-  },
+define: {
+  'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+  'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+},
 });
